a0885588cd4db8285f231b8258806ed1d3dbed83
Ricky Zhang <85967741+fish-mouse@users.noreply.github.com>
2026-04-08 00:05:14 +0000

    Adding system monitor and race data topic for post race analysis

--- bringup.launch.py
+++ bringup.launch.py
@@ -3,7 +3,9 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
 from ament_index_python.packages import get_package_share_directory
 import os
 
@@ -15,9 +17,42 @@ def generate_launch_description() -> LaunchDescription:
     f1tenth_launch_dir = os.path.join(
         get_package_share_directory("f1tenth_stack"), "launch"
     )
+    
+    record_bag = LaunchConfiguration("record_bag")
+    bag_dir = LaunchConfiguration("bag_dir")
+    bag_name = LaunchConfiguration("bag_name")
+
+    bag_topics = [
+       "/drive",
+       "/scan",
+       "/tf",
+       "/tf_static",
+       "/odom",
+       "/joy",
+       '/diagnostics'
+    ]
+
+    bag_record = ExecuteProcess(
+              condition=IfCondition(record_bag),
+              cmd=[
+                    "ros2", "bag", "record",
+                    "-o", PathJoinSubstitution([bag_dir, bag_name]),
+
+                    *bag_topics,
+              ],
+              output="screen",
+    )
+
 
     return LaunchDescription(
-        [
+        [   
+            # (new) args:
+            DeclareLaunchArgument("record_bag", default_value="false"),
+            DeclareLaunchArgument("bag_dir", default_value="/race_ws/bags"),
+
+            # (new) recorder:
+            bag_record,
+
             # Base drive stack: joy_node, joy_teleop, mux, ackermann_to_vesc, vesc_driver.
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
@@ -32,6 +67,24 @@ def generate_launch_description() -> LaunchDescription:
                 PythonLaunchDescriptionSource(
                     os.path.join(livox_launch_dir, "msg_MID360_launch.py")
                 )
+            ),
+            # System monitor (publishes diagnostics)
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(
+                        get_package_share_directory("ros2_system_monitor"),
+                        "launch",
+                        "system_monitor.launch.py",
+                    )
+                ),
+                # Optional: disable monitors you don't want
+                launch_arguments={
+                    # "enable_cpu_monitor": "true",
+                    # "enable_hdd_monitor": "true",
+                    # "enable_mem_monitor": "true",
+                    # "enable_net_monitor": "true",
+                    # "enable_ntp_monitor": "false",
+                }.items(),
             ),
             # PointCloud2 -> LaserScan (Livox to /scan)
             Node(
